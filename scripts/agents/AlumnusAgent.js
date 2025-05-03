cbad31962af85b3204f6e32b7197ff790d3691a4
OT-Lyz <150920750+OT-Lyz@users.noreply.github.com>
2025-05-04 02:28:01 +0800

    Update AlumnusAgent.js

--- scripts/agents/AlumnusAgent.js
+++ scripts/agents/AlumnusAgent.js
@@ -5,9 +5,7 @@ class AlumnusAgent extends BaseAgent {
     this.major = "Computer Science";
   }
 
-  generateAlumniPrompt(userInput) {
-    return `${userInput} 
-            [Note: You graduated from ${this.school} with a degree in ${this.major}. 
-            Casually mention this connection]`;
+  generateAlumniPrompt(input) {
+    return `${input}\n[Context: You graduated from ${this.school}. Mention casually]`;
   }
 }
