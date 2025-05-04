0b197414c0fd124a984d6a161dcb97b474365c04
OT-Lyz <150920750+OT-Lyz@users.noreply.github.com>
2025-05-04 15:43:19 +0800

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,2 +1,2 @@
-const DEEPSEEK_API_KEY = 'sk-9f2b2651659145cb9573c3659424f26b';
-const DEEPSEEK_API_URL = 'https://api.deepseek.com/chat/completions';
+export const API_KEY = 'your-key';
+export const API_URL = 'https://api.deepseek.com/v1/chat/completions';
