ea8afa9c378b3cf3420159346dedccf5ee2338bc
OT-Lyz <150920750+OT-Lyz@users.noreply.github.com>
2025-05-04 15:30:20 +0800

    Delete scripts/utils/formatter.js

--- scripts/utils/formatter.js
+++
@@ -1,8 +0,0 @@
-export function formatResponse(apiResponse) {
-  let text = apiResponse.choices[0].message.content;
-  // 安全过滤
-  CONFIG.SAFETY_FILTERS.forEach(filter => {
-    text = text.replace(filter.regex, filter.replace);
-  });
-  return text;
-}
