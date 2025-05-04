7f15b11013ea295bdb32292ee4ea0c82607f6adb
OT-Lyz <150920750+OT-Lyz@users.noreply.github.com>
2025-05-04 15:26:30 +0800

    Update AlumnusAgent.js

--- scripts/agents/AlumnusAgent.js
+++ scripts/agents/AlumnusAgent.js
@@ -1,11 +1,7 @@
-class AlumnusAgent extends BaseAgent {
+import { BaseAgent } from './BaseAgent.js';
+
+export class AlumnusAgent extends BaseAgent {
   constructor() {
     super();
-    this.school = "Stanford University";
-    this.major = "Computer Science";
-  }
-
-  generateAlumniPrompt(input) {
-    return `${input}\n[Context: You graduated from ${this.school}. Mention casually]`;
   }
 }
